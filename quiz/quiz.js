99ea6875a6f7fc8e06b66d95d6b4cc4a061960ff
MustafaAdel <mustafaadelsoftwaredeveloper@gmail.com>
2022-11-27 05:00:23 +0200

    make some classes like quiz and final plus enhance others

---
+++ quiz/quiz.js
@@ -0,0 +1,13 @@
+class Quiz {
+  constructor(quizElement, amount, question) {
+    this.quizElement = quizElement;
+    this.currentElement = document.querySelector(".current");
+    this.totalElement = document.querySelector(".total");
+    this.finalElement = document.querySelector(".final");
+    this.nextBtn = document.querySelector("#next");
+    this.totalAmount = amount;
+    this.answeredAmount = 0;
+  }
+}
+
+export default Quiz;
